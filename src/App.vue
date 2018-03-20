d273a16d40d86fb4c85d498a8c205b0663d71f9a
Zhao Lingxiao <zlx_120534@163.com>
2018-03-20 11:30:06 +0800

    音乐播放器项目代码初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div#app
+    MHeader
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from '@/components/m-header/m-header'
+
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~@/common/stylus/variable'
+
+#app
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  color $color-theme
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div.m-header
+    div.icon
+    h1.text Chicken Music
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'm-header'
+}
+</script>
+
+<style lang="stylus">
+@import '~@/common/stylus/variable';
+@import '~@/common/stylus/variable';
+</style>
